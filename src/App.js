ab1fa2d8f86b284c9add05785b0b09f0b104ed65
John Wee <johnwee@Johns-MacBook-Pro.local>
2024-04-06 21:05:03 +0800

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import firebase from './firebabase-config';
+function App() {
+  console.log(firebase)
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
