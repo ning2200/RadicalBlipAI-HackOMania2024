5f2dd8b9dfac40aa1b160c611a8e8bc3c015f8c1
Logic <31919914+LogicUI@users.noreply.github.com>
2024-04-07 06:11:34 +0800

    changes to pie chart

--- src/chartScreens/CompareScreen/CompareScreen.js
+++ src/chartScreens/CompareScreen/CompareScreen.js
@@ -27,12 +27,22 @@ const CompareScreen = ({
               marginTop: "20px",
             }}
           >
-            <div style={{ fontSize: "20px" }}>Similar Companies?</div>
+            <div
+              style={{
+                fontSize: "18px",
+                fontWeight: "600",
+                fontFamily: "Raleway",
+              }}
+            >
+              Similar Companies?
+            </div>
             <div
               style={{
                 fontSize: "12px",
                 marginTop: "8px",
-                fontWeight: "medium",
+                fontWeight: "400",
+                fontFamily: "Raleway",
+                fontSize: "13px",
               }}
             >
               Please select the option you wish to compare with
@@ -50,8 +60,8 @@ const CompareScreen = ({
               style={{
                 marginTop: "20px",
                 display: "flex",
-                width: "50%",
-                justifyContent: "space-between",
+                width: "95%",
+                justifyContent: "flex-start",
                 flexWrap: "wrap",
               }}
             >
@@ -61,8 +71,14 @@ const CompareScreen = ({
                   style={{
                     borderRadius: "5px",
                     marginBottom: "10px",
-                    background: company.selected ? "#000000" : "#DFDFDF",
-                    color: company.selected ? "white" : "#CACACA",
+                    marginRight: "5px",
+                    height: "50px",
+                    width: "270px",
+                    background: company.selected ? "#43473E" : "#DFDFDF",
+                    color: company.selected ? "#F0F0F0" : "#CACACA",
+                    fontSize: "14px",
+                    fontWeight: "400",
+                    fontFamily: "Raleway",
                   }}
                   onClick={() => {
                     onCompanyClick(company.companyName);
@@ -82,84 +98,13 @@ const CompareScreen = ({
     <div
       style={{
         width: "60%",
-       
-        // marginTop: "30px",
         background: "#F2F2F2",
         padding: "30px",
-<<<<<<< HEAD
         borderRadius: "20px",
         boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
       }}
     >
-      {
-        <CompareCompanies
-          listOfCompanies={listOfCompanies}
-          mockData={mockData}
-        />
-      }
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: "20px",
-        }}
-      >
-        <div style={{ fontSize: "18px", fontWeight:"600", fontFamily:"Raleway"}}>Similar Companies?</div>
-        <div
-          style={{ fontSize: "12px", marginTop: "8px", fontWeight:"400", fontFamily:"Raleway", fontSize:"13px"}}
-        >
-          Please select the option you wish to compare with
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-        }}
-      >
-        <div
-          style={{
-            marginTop: "20px",
-            display: "flex",
-            width: "95%",
-            justifyContent: "flex-start",
-            flexWrap: "wrap",
-
-          }}
-        >
-          {listOfCompanies.map((company) => (
-            <Button
-              key={company.companyName}
-              style={{
-                borderRadius: "5px",
-                marginBottom: "10px",
-                marginRight: "5px",
-                height: "50px",
-                width: "270px",
-                background: company.selected ? "#43473E" : "#DFDFDF",
-                color: company.selected ? "#F0F0F0" : "#CACACA",
-                fontSize: "14px", fontWeight:"400", fontFamily:"Raleway"
-              }}
-              onClick={() => {
-                onCompanyClick(company.companyName);
-              }}
-            >
-              {company.companyName}
-            </Button>
-          ))}
-        </div>
-      </div>
-=======
-        borderRadius: "5%",
-        minHeight:'600px',
-        boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
-      }}
-    >
       {renderChartByView(selectedButton)}
->>>>>>> c9e38f7 (changes to pie chart)
     </div>
   );
 };
